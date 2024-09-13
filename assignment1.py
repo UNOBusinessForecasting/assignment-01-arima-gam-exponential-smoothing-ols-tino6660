174588e272b74ea2bd4a56628e42ff0a35677b32
tino6660 <martingleason@unomaha.edu>
2024-09-12 20:22:01 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,16 +1,10 @@
 import pandas as pd
-from pygam import LinearGAM, s, f, l
+import statsmodels.formula.api as smf
 import numpy as np
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-Titles = ['year','month','day','hour']
-x = data[Titles]
-y = data[['trips']]
+model = smf.ols("Trips ~ hour", data=data
+modelFit = model.fit() 
 
-model = LinearGAM(s(0) + s(1) + s(2) + s(3))
-model = model.gridsearch(x.values, y)
-
-modelFit = model.fit(x,y) 
-
-pred = modelFit.predict(data.loc[4, Titles].values.reshape(1,4))
+pred = modelFit.predict()

a28ed23b14b3f6ecf62cbe2c437fc920a7838f5a
tino6660 <martingleason@unomaha.edu>
2024-09-12 19:02:51 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -3,7 +3,6 @@ from pygam import LinearGAM, s, f, l
 import numpy as np
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
-data['Timestamp'] = pd.to_datetime(data['Timestamp'])
 
 x = data[['month','day','hour']]
 y = data[['trips']]

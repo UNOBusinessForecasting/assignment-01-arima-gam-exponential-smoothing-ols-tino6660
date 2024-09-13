0ba6e18e02e38b72907111ad63645dc76cf79fa8
tino6660 <martingleason@unomaha.edu>
2024-09-12 20:24:33 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-model = smf.ols("Trips ~ hour", data=data)
+model = smf.ols("trips ~ hour", data=data)
 modelFit = model.fit() 
 
 pred = modelFit.predict()

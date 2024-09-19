8ebfe07920fd1ce4f969271dfe6b3f43228bc2c1
tino6660 <martingleason@unomaha.edu>
2024-09-19 03:13:16 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pygam import LinearGAM, s, f, l
 import numpy as np
 import patsy as pt
+from statsmodels.tsa.api import SimpleExpSmoothing
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
@@ -12,7 +13,7 @@ y, x = pt.dmatrices(eqn, data=data)
 model = LinearGAM(s(0) + f(1) + f(2) + f(3))
 model = model.gridsearch(np.asarray(x), y)
 
-modelFit = model.fit(x,y)
+modelFit = SimpleExpSmoothing(model).fit
 
 data2 = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 

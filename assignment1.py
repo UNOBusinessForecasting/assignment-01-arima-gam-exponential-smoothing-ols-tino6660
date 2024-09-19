d8aec94ba4d8bd125b6076728ad2fb1a9012c830
tino6660 <martingleason@unomaha.edu>
2024-09-19 01:58:51 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from pygam import LinearGAM, s, f, l
 import numpy as np
+from scipy import stats
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
 data['Timestamp']= pd.to_numeric(pd.to_datetime(data['Timestamp']))
-x = data[['year', 'month', 'day', 'hour']]
-y = data['trips']
+data = input_string.replace(',', '') 
+data1 = float(data) 
+x = data1[['year', 'month', 'day', 'hour']]
+y = data1['trips']
 
 model = LinearGAM(s(0) + f(1) + f(2) + f(3))
 model = model.gridsearch(x.values, y)

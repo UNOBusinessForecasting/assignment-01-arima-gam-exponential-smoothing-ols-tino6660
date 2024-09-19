286bad15306a919f786b5c8909935ab227ff314f
tino6660 <martingleason@unomaha.edu>
2024-09-19 00:40:39 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -11,6 +11,8 @@ y = data[['trips']]
 model = LinearGAM(s(0) + s(1) + s(2) + s(3))
 model = model.gridsearch(x.values, y)
 
+modelFit = model.fit(x,y)
+
 data2 = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
 data2['Trips'] = ""

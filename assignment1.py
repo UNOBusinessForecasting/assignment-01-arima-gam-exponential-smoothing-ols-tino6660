c9f9edf450ec5da05c19d62be403a3931ecddc13
tino6660 <martingleason@unomaha.edu>
2024-09-19 02:25:41 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -6,22 +6,19 @@ from scipy import stats
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
 data['Timestamp']= pd.to_numeric(pd.to_datetime(data['Timestamp']))
-data_edit = data.replace(',', '') 
-data1 = float(data_edit) 
-x = data1[['year', 'month', 'day', 'hour']]
-y = data1['trips']
+x = data[['year', 'month', 'day', 'hour']]
+y = data['trips']
+x1 = float(x)
+y1 = float(y)
 
 model = LinearGAM(s(0) + f(1) + f(2) + f(3))
-model = model.gridsearch(x.values, y)
+model = model.gridsearch(x1.values, y1)
 
 modelFit = model.fit(x,y)
 
 data2 = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
 data2['Timestamp']= pd.to_numeric(pd.to_datetime(data2['Timestamp']))
-data2_edit = data.replace(',', '') 
-data3 = float(data_edit2) 
+data2['Trips'] = ""
 
-data3['Trips'] = ""
-
-pred = modelFit.predict(data3)
+pred = modelFit.predict(data2)

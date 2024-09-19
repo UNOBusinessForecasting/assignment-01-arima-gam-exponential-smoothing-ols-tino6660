732063f26180e810b0ea93e5e76d55b9d47f7348
tino6660 <martingleason@unomaha.edu>
2024-09-19 00:37:31 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,12 @@ import numpy as np
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-model = smf.ols("trips ~ month + day + hour", data=data)
-modelFit = model.fit() 
+Titles = ['year','month','day','hour']
+x = data[Titles]
+y = data[['trips']]
+
+model = LinearGAM(s(0) + s(1) + s(2) + s(3))
+model = model.gridsearch(x.values, y)
 
 data2 = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 

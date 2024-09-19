311f8269914774bb14a74da08de7a422409d1b16
tino6660 <martingleason@unomaha.edu>
2024-09-19 00:17:32 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -7,7 +7,7 @@ data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main
 model = smf.ols("trips ~ month + day + hour", data=data)
 modelFit = model.fit() 
 
-data2 = pd.read_csv("https://github.com/UNOBusinessForecasting/assignment-01-arima-gam-exponential-smoothing-ols-tino6660/blob/main/assignment_data_train.csv")
+data2 = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
 df = pd.DataFrame(data2, columns=['Timestamp', 'month', 'day', 'hour', 'trips'])
 

b785326e73102d8a71d992e957b2d212c8abf3bc
tino6660 <martingleason@unomaha.edu>
2024-09-19 00:38:54 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import statsmodels.formula.api as smf
+from pygam import LinearGAM, s, f, l
 import numpy as np
 
 data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
